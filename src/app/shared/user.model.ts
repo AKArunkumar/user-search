8078c6a6fab8d1847238a962dffeef053da45810
AKArunkumar <arunkumar717.at@gmail.com>
2018-06-05 20:21:16 +0530

    defined user model

---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    name          : string;
+    id            : number;
+    avatar_ur     : string;
+    html_url      : string;
+    followers_url : string;
+    following_url : string;
+    repos_url     : string;
+    score         : number;
+}
