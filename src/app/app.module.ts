684812fa352554ba0e80dbc5fa7ecefff43f09fc
AKArunkumar <arunkumar717.at@gmail.com>
2018-06-05 20:22:01 +0530

    imported userservice in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserfilterPipe } from './pipes//userfilter.pipe';
+import { UserService } from './shared//serives/user.service';
+import { UserShowComponent } from './compenets/user-show/user-show.component';
+import { SearchBarComponent } from './compenets/search-bar/search-bar.component';
+import { UserComponent } from './compenets/user-show/user/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserfilterPipe,
+    UserShowComponent,
+    SearchBarComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
