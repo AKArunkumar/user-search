eb5d12e04a6e1c1d2de7dfb1746a6fb08b0fbf68
AKArunkumar <arunkumar717.at@gmail.com>
2018-06-06 12:34:24 +0530

    added route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserfilterPipe } from './pipes//userfilter.pipe';
 import { UserService } from './shared//serives/user.service';
-import { UserShowComponent } from './compenets/user-show/user-show.component';
 import { SearchBarComponent } from './compenets/search-bar/search-bar.component';
 import { UserComponent } from './compenets/user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserfilterPipe,
-    UserShowComponent,
     SearchBarComponent,
     UserComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
